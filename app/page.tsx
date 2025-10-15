f1ec85661963d52e0960f05c70f857703fc5a83f
Mayank Mahajan <mayankmahajan9056@gmail.com>
2025-10-16 01:23:03 +0530

    initial project setup with nextjs and shadcn ui library

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <Button className=''>Start</Button>
+    </div>
+  )
+}
+
+export default Home
