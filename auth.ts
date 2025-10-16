bccf8943eabdc715314bcce0cd326a692065193f
Mayank Mahajan <mayankmahajan9056@gmail.com>
2025-10-16 17:04:44 +0530

    authjs implement for authorization

---
+++ auth.ts
@@ -0,0 +1,6 @@
+import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+// import { prisma } from "@/prisma"
+ 
+export const { handlers, auth, signIn, signOut } = NextAuth({
+})
